e9ee42149acb5950b4edd82efa3b2fae7fb37665
unknown <fabioclericuzilima@hotmail.com>
2024-02-22 20:58:23 -0300

    Tratamento de exceções da aplicação, importação dos arquivos para o ambiente de desenvolvimento e alterações nos templates/views

--- django1/settings.py
+++ django1/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n2-9#vgiz_qllh_7@yhm1&u=u5x%i)@2*-r+)7l8xzcm(ywo+g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# Se o DEBUG estiver true, a aplicação está em modo de desenvolvimento
+# Se o DEBUG estiver no modo false, estamos no ambiente de produção
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
@@ -116,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' #configuração utilizada durante o desenvolvimento
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #Usado durante a produção
+LOGOUT_REDIRECT_URL = 'index'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
